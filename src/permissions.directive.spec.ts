49f2aef8d806cb64e8f7967250217e544b93ac18
Oleksandr Khymenko <Oleksandr.Khymenko@evry.com>
2017-08-17 20:14:44 +0300

    Add tests for permissions directive

--- src/permissions.directive.spec.ts
+++ src/permissions.directive.spec.ts
@@ -1,8 +1,95 @@
 import { PermissionsDirective } from './permissions.directive';
+import { Component } from '@angular/core';
+import { NgxPermissionsModule } from './index';
+import { TestBed } from '@angular/core/testing';
+import { PermissionsService } from './permissions.service';
+
+enum PermissionsTestEnum {
+    ADMIN = <any> 'ADMIN',
+    GUEST = <any> 'GUEST'
+}
 
 describe('PermissionsDirective', () => {
     it('should create an instance', () => {
         // const directive = new PermissionsDirective();
         expect(true).toBeTruthy();
     });
+});
+
+describe('Permission directive angular except', () => {
+    @Component({selector: 'test-comp',
+        template: `<ng-template permissions [permissionsExcept]="'ADMIN'"><div>123</div></ng-template>`})
+    class TestComp {
+        data: any;
+    }
+
+    let permissionService;
+    let permissions;
+    let fixture;
+    let comp;
+    beforeEach(() => {
+        TestBed.configureTestingModule({declarations: [TestComp], imports: [NgxPermissionsModule.forRoot()]});
+
+        fixture = TestBed.createComponent(TestComp);
+        comp = fixture.componentInstance;
+
+        permissionService = fixture.debugElement.injector.get(PermissionsService);
+
+    });
+
+
+    it('Should not show component', () => {
+        permissionService.loadPermissions([PermissionsTestEnum.ADMIN, PermissionsTestEnum.GUEST]);
+
+        fixture.detectChanges();
+        let content = fixture.debugElement.nativeElement.querySelector('div');
+        expect(content).toEqual(null);
+    })
+    it ('Should show the component', () => {
+        permissionService.loadPermissions([PermissionsTestEnum.GUEST]);
+
+        fixture.detectChanges();
+        let content = fixture.debugElement.nativeElement.querySelector('div');
+        expect(content).toBeTruthy();
+        expect(content.innerHTML).toEqual('123');
+    })
+});
+
+describe('Permission directive angular only', () => {
+    @Component({selector: 'test-comp',
+        template: `<ng-template permissions [permissionsOnly]="'ADMIN'"><div>123</div></ng-template>`})
+    class TestComp {
+        data: any;
+    }
+
+    let permissionService;
+    let permissions;
+    let fixture;
+    let comp;
+    beforeEach(() => {
+        TestBed.configureTestingModule({declarations: [TestComp], imports: [NgxPermissionsModule.forRoot()]});
+
+        fixture = TestBed.createComponent(TestComp);
+        comp = fixture.componentInstance;
+
+        permissionService = fixture.debugElement.injector.get(PermissionsService);
+
+    });
+
+
+    it('Should show the component', () => {
+        permissionService.loadPermissions([PermissionsTestEnum.ADMIN, PermissionsTestEnum.GUEST]);
+
+        fixture.detectChanges();
+        let content = fixture.debugElement.nativeElement.querySelector('div');
+        expect(content).toBeTruthy();
+        expect(content.innerHTML).toEqual('123');
+    });
+    it ('Should not show the component', () => {
+        permissionService.loadPermissions([PermissionsTestEnum.GUEST]);
+
+        fixture.detectChanges();
+        let content = fixture.debugElement.nativeElement.querySelector('div');
+        expect(content).toEqual(null);
+    })
 });
